e7109b05ecad117489b7039c00d2a7b2069bc236
calebavner <avner.trabalho@hotmail.com>
2025-04-03 18:43:45 -0300

    Criação do projeto

---
+++ src/main/java/app/AppRenderApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRenderApplication.class, args);
+	}
+
+}
